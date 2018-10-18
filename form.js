7600a98eb1a4348d1c23f485df1a70489740fa62
xieht <xieht@glodon.com>
2018-10-18 18:02:27 +0800

    update form.js

--- form.js
+++ form.js
@@ -173,20 +173,54 @@ option.value;
 	var text = selectbox.option[0].value;
 
 //选择选项
+selectbox.selectedIndex
+	var selectedOption = selectbox.option[selectbox.selectedIndex]; 
+	//对于只允许选择一项的选择框，可以使用选择框的selectedIndex属性选中
+	//对于只多选选择框，设置selectedIndex会导致取消以前的所有选项并选择指定的那一项，读取selectedIndex则只会返回选中项中的第一项
+	selectbox.options[0].selected = true;
+	//设置第一个option为选中项，单选选择框会取消其他已选项选择。
 
 
 
 //添加选项
+//方法1，DOM常规方式
+var newOption = document.createElement("option");
+newOption.appendChild(document.createTextNode("Option text"));
+newOptioin.setAttribute("value", "Option value");
+selectbox.appendChild(newOption);
 
+//方法2, 使用Option构造函数
+var newOption = new Option("Option text", "Option value");
+selectBox.appendChild(newOption); //在ie8及之前的版本中有问题
 
+//方法3，使用选择框的add()方法，最佳方案
+var newOption = new Option("Option text", "Option value");
+selectBox.add(newOption, undefined);  //第二个参数为undefined，新选项插入到列表最后
 
 //移除选项
+//方法1，DOM的removerChild()
+selectbox.removeChild(selectbox.option[0]); //移除第一项
+
+//方法2，使用选择框的remove()方法
+selectbox.remove(0); //移除第一项
+
+//方法3，将相应选项设置为null
+selectbox.option[0] = null //移除第一项
 
 
 
+//移动和重排选项
+//移动选项使用appendChild(),DOM appendChild()方法传入一个文档中已有元素，那么会从该元素父节点中自动移除它，再把它添加到指定位置。
+var selectbox1 = document.getElementById("selLocations1");
+var selectbox2 = document.getElementById("selLocations2");
+selectbox2.appendChild(selectbox1.option[0]);
 
-//移动和重拍选项
+//重排选项使用insertBefore(),
+var optionToMove = selectbox.options[1];
+selectbox.insertBefore(optionToMove, selectbox.options[optionToMove.index - 1]); //在选项框中向前移动一个选项的位置
 
+var optionToMove = selectbox.options[1];
+selectbox.insertBefore(optionToMove, selectbox.options[optionToMove.index + 2]); //在选项框中向后移动一个选项的位置
 
 
 
